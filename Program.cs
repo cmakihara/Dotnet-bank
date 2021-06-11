902d1f5ad90f1828076da1cf9b83dc111490963e
Makihara <clmakihara@gmail.com>
2021-06-11 15:31:30 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DIO.Series
+{
+	public class pizzaria
+	{
+		public static void Main()
+		{
+            int timeInSeconds = int.Parse(Console.ReadLine());
+
+       //continue a solucao
+            var hours = 0;      
+            var minutes =  0 ;
+            var seconds =   0 ;
+
+			Console.WriteLine("1: " + timeInSeconds);
+        
+			while (timeInSeconds >= 60)
+			{
+				minutes += 1;
+                timeInSeconds -= 60;
+						
+				 if (minutes == 60)
+				{
+					hours += 1;
+					minutes = 0;
+				}
+				if(timeInSeconds < 60 && timeInSeconds > 0)
+				{
+					seconds = timeInSeconds;
+				}				
+			}            
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
